57e3707469caf00c5fbfadc047dede33d817b34b
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-07-29 16:09:30 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -28,8 +28,8 @@ from config import Config
 REPLY_MESSAGE=Config.REPLY_MESSAGE
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://heroku.com/deploy?template=https://github.com/subinps/MusicPlayer'),
-        InlineKeyboardButton('🧩 Source Code', url='https://github.com/subinps/MusicPlayer'),
+        InlineKeyboardButton('⚡️Make Own Bot', url='https://heroku.com/deploy?template=https://github.com/OxyNotOp/OxyVCbot'),
+        InlineKeyboardButton('🧩 Source Code', url='https://github.com/OxyNotOp/OxyVCbot'),
     ],
     [
         InlineKeyboardButton('🎧Play Music', url=f'https://t.me/{USERNAME}'),
